b42eed419a6e31eaae78523faf0bf1763697e313
Edward A Maxedon Sr <edadma@gmail.com>
2020-04-11 21:13:47 -0400

    startt coding basic expression code generation

--- src/main/scala/CodeGenerator.scala
+++ src/main/scala/CodeGenerator.scala
@@ -5,33 +5,74 @@ object CodeGenerator {
   def apply(as: List[SourceAST]) = {
     val out = new StringBuilder
 
-    def line(s: String, indent: Int = 0) = {
-      out ++= "  " * indent
+    def line(s: String) = {
       out ++= s
       out += '\n'
     }
 
-    compileSource(as.head)
-
-    def compileSource(src: SourceAST) = {
-      src.stmts map compileStatement
+    def indent(s: String) = {
+      out ++= "  "
+      line(s)
     }
 
-    def compileStatement(stmt: StatementAST): Unit =
+    compileSource(as.head)
+
+    def compileSource(src: SourceAST) =
+      src.stmts foreach compileTopLevelStatement
+
+    def compileTopLevelStatement(stmt: StatementAST): Unit =
       stmt match {
-        case t: ExpressionAST           =>
-        case DeclarationBlockAST(decls) => decls foreach compileStatement
+        case DeclarationBlockAST(decls) => decls foreach compileTopLevelStatement
         case DefAST(dpos, name, FunctionPieceAST(ppos, parms, arb, parts, where)) =>
-          line("define i32 @" ++ name ++ "() {")
-          compileExpression(parts.head.body, 0)
-          line("ret i32 0", 1)
-          line("}")
+          compileFunction(name, parms, arb, parts, where)
       }
 
-    def compileExpression(expr: ExpressionAST, start: Int): Int =
-      expr match {
-        case BlockExpressionAST(stmts) =>
+    def compileFunction(name: String,
+                        parms: List[PatternAST],
+                        arb: Boolean,
+                        parts: List[FunctionPart],
+                        where: List[DeclarationStatementAST]) = {
+      val counter = new Counter
+
+      def operation(s: String) = indent(s"%${counter.next} = $s")
+
+      def compileStatement(stmt: StatementAST): Unit =
+        stmt match {
+          case t: DeclarationStatementAST =>
+          case t: ExpressionAST           => compileExpression(t)
+        }
+
+      def compileExpression(expr: ExpressionAST): Int = {
+        expr match {
+          case BlockExpressionAST(stmts)    => stmts foreach compileStatement
+          case LiteralExpressionAST(v: Int) => operation(v.toString)
+        }
+
+        counter.current
       }
+
+      line("define i32 @" ++ name ++ "() {")
+      compileExpression(parts.head.body)
+      indent("ret i32 0")
+      line("}")
+    }
   }
+
+}
+
+class Counter {
+
+  var count = 0
+
+  def next = {
+    count += 1
+    count
+  }
+
+  def current =
+    if (count == 0)
+      sys.error("no last count")
+    else
+      count - 1
 
 }
